513b5a3c44da33310160ef062480d4104c5f88ef
Svintsitskiy Vlad <vsvintsitskiy@yandex.ru>
2021-11-05 17:03:59 +0300

    rdbms

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
@@ -1,7 +1,62 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
+
 class TransferOptimisticLock {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/db",
+            "postgres",
+            "postgres"
+        )
+        var currentVersion: Int
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            try {
+                conn.autoCommit = false
+
+                val preparedStatement1 = conn.prepareStatement(
+                    "select * from account1 where id = $accountId1"
+                )
+
+                preparedStatement1.executeQuery().use {
+                    it.next()
+                    if (it.getInt("amount") - amount < 0)
+                        throw Exception()
+                    currentVersion = it.getInt("version")
+                }
+
+                val preparedStatement2 =
+                    conn.prepareStatement(
+                        "update account1 set amount = amount - $amount, version = version + 1 " +
+                                "where id = $accountId1 and version = $currentVersion"
+                    )
+
+                var result = preparedStatement2.executeUpdate()
+                if (result == 0)
+                    throw Exception()
+
+                val prepareStatement3 =
+                    conn.prepareStatement("select * from account1 where id = $accountId2")
+                prepareStatement3.executeQuery().use { statement ->
+                    statement.next()
+                    currentVersion = statement.getInt("version")
+                }
+                val prepareStatement4 =
+                    conn.prepareStatement(
+                        "update account1 set amount = amount + $amount, version = version + 1 " +
+                                "where id = $accountId2 and version = $currentVersion"
+                    )
+                result = prepareStatement4.executeUpdate()
+                if (result == 0)
+                    throw Exception()
+                conn.commit()
+            } catch (exception: Exception) {
+                println(exception.message)
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
 }
--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferPessimisticLock.kt
@@ -1,7 +1,51 @@
 package ru.sber.rdbms
 
+import java.lang.Long.max
+import java.lang.Long.min
+import java.sql.DriverManager
+
 class TransferPessimisticLock {
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        val connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/db",
+            "postgres",
+            "postgres"
+        )
+
+        val minId = min(accountId1, accountId2)
+        val maxId = max(accountId1, accountId2)
+
+        connection.use { conn ->
+            val autoCommit = conn.autoCommit
+            try {
+                conn.autoCommit = false
+                val prepareStatement1 = conn.prepareStatement("select * from account1 where id = $minId for update")
+                prepareStatement1.use { statement ->
+                    statement.executeQuery()
+                }
+                val prepareStatement2 =
+                    conn.prepareStatement("select * from account1 where id = $maxId for update")
+                prepareStatement2.use { statement ->
+                    statement.executeQuery()
+                }
+
+                val prepareStatement3 =
+                    conn.prepareStatement("update account1 set amount = amount - $amount where id = $accountId1")
+                prepareStatement3.use { statement ->
+                    statement.executeUpdate()
+                }
+                val prepareStatement4 =
+                    conn.prepareStatement("update account1 set amount = amount + $amount where id = $accountId2")
+                prepareStatement4.use { statement ->
+                    statement.executeUpdate()
+                }
+                conn.commit()
+            } catch (exception: Exception) {
+                println(exception.message)
+                conn.rollback()
+            } finally {
+                conn.autoCommit = autoCommit
+            }
+        }
     }
 }
